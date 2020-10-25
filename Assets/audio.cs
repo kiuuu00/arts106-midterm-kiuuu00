02df59ae64598d3a94640033f2b2779cbbdf6faa
Dk <70618651+kiuuu00@users.noreply.github.com>
2020-10-24 19:43:48 -0600

    problems with Audio

---
+++ Assets/audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audio : MonoBehaviour
+{
+
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot room1Snapshot;
+
+    private void OnTriggerEnter(collider other)
+    {
+
+        if (other.CompareTag("Enemyzone"))
+        {
+            room1Snapshot.TransitionTo(0.5f);
+        }
+    }
+    private void OnTriggerExit(collider other)
+    {
+
+        if (other.CompareTag("Enemyzone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+    }
+}

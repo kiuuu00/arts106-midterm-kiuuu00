5c82b6c095c3521e8c90ce1564a3a386a4104afe
Dkrv <70618651+kiuuu00@users.noreply.github.com>
2020-12-14 15:08:40 -0600

    1
    I didn't think that the baked lightning was too bright, but there are 4 spotlights if I'm not wrong, underwater under the fog.

---
+++ Assets/UIMAINMENU.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMAINMENU : MonoBehaviour
+{
+    public Animator myanimator;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MidtermProject");
+    }
+
+    public void Hidbutton()
+    {
+        myanimator.SetBool("ISH", true);
+        Invoke("StartGame", 1.0f);
+    }
+}
---
+++ Assets/UIMANAGER.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public Animator startButton;
+    public Animator settingsButton;
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    public Animator dialog;
+
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
+    public Animator gearImage;
+    public Animator contentPanel;
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+
+        {
+            //..skipped..
+
+            gearImage.SetBool("isHidden", !isHidden);
+        }
+    }
+}
